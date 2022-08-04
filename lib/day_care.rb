8e71668195d58835e858f70f40f0b22c87f9df0c
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-08-03 19:02:16 -0600

    finished IC practice

---
+++ lib/day_care.rb
@@ -0,0 +1,27 @@
+class DayCare
+  attr_reader :name,
+              :customers
+
+  def initialize(name)
+    @name = name
+    @customers = []
+  end
+
+  def add_customer(customer)
+    @customers << customer
+  end
+
+  def find_by_id(id)
+    cust = []
+    @customers.each do |customer|
+      if customer.id == id
+        cust << customer
+      end
+    end
+    cust.first.name
+  end
+
+  def un_fed_pets
+    @customers.first.pets.select { |pet|  pet.fed? == false }
+  end
+end
--- spec/day_care_spec.rb
+++ spec/day_care_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe DayCare do
   before :each do
     @day_care1 = DayCare.new("Pup Spa")
     @day_care2 = DayCare.new("Kitty Paradise")
+
+    @joel = Customer.new("Joel", 2)
+
+    @samson = Pet.new({name: "Samson", type: :dog})
+    @lucy = Pet.new({name: "Lucy", type: :cat})
+
+    @joel.adopt(@samson)
+    @joel.adopt(@lucy)
   end
 
   it 'exists' do
@@ -14,7 +22,27 @@ RSpec.describe DayCare do
   end
 
   it 'has a name' do
-    expect(@day_care1.name).to be_instance_of("Pup Spa")
-    expect(@day_care2.name).to be_instance_of("Kitty Paradise")
+    expect(@day_care1.name).to eq("Pup Spa")
+    expect(@day_care2.name).to eq("Kitty Paradise")
+  end
+
+  it 'has no customers by default' do
+    expect(@day_care1.customers).to eq([])
+    expect(@day_care2.customers).to eq([])
+  end
+
+  it 'can have customers with pets' do
+    @day_care1.add_customer(@joel)
+    expect(@day_care1.customers).to eq([@joel])
+  end
+
+  it 'customer can be found by id' do
+    @day_care1.add_customer(@joel)
+    expect(@day_care1.find_by_id(2)).to eq("Joel")
+  end
+
+  it 'can return the amount of pets that are unfed' do
+    @day_care1.add_customer(@joel)
+    expect(@day_care1.un_fed_pets).to eq([@samson, @lucy])
   end
 end
